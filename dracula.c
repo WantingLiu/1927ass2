f77c5fede6c08ee300726b79a32bac80a72f415e
Tian Ling <awrayan@gmail.com>
2014-10-20 22:04:40 +1100

    Fixed Dracula to take into account start of game, fixed indentation.

--- dracula.c
+++ dracula.c
@@ -19,43 +19,46 @@ void decideDraculaMove(DracView gameState)
    int *paths = whereCanIgo(gameState, &numLocations, TRUE, FALSE);
 
    // If Dracula has at least 1 legal move
-   if (numLocations > 0) {
-      Graph gameMap = newGraph(NUM_MAP_LOCATIONS);
-      // Only dealing with road connections for now
+	if (numLocations > 0 && whereIs(gameState, PLAYER_DRACULA) != -1) {
+		Graph gameMap = newGraph(NUM_MAP_LOCATIONS);
+	   // Only dealing with road connections for now
 //Bernice comments: I wonder if we can just get the map by gameState->view->map (where 'map' is a Graph value in GameView.c and 'view' is a gameView value in HunterView.c)
 //   if this was possible, we may not have to generate and populate a local graph everytime
-      addRoadConnections(gameMap);
+		addRoadConnections(gameMap);
 
-      int i, minimum, tempMinimum; // Minimum stores the distance of the closest hunter
-      PlayerID closestHunter = PLAYER_LORD_GODALMING;
-      minimum = findPathDist(gameMap, whereIs(gameState, PLAYER_DRACULA), whereIs(gameState, PLAYER_LORD_GODALMING)); // Defaulting to PLAYER_LORD_GODALMING initially
-
-      // Getting distance to each hunter to priortise which one to run from
+		int i, minimum, tempMinimum; // Minimum stores the distance of the closest hunter
+		PlayerID closestHunter = PLAYER_LORD_GODALMING;
+		minimum = findPathDist(gameMap, whereIs(gameState, PLAYER_DRACULA), whereIs(gameState, PLAYER_LORD_GODALMING)); // Defaulting to PLAYER_LORD_GODALMING initially
+	  // Getting distance to each hunter to priortise which one to run from
 //Bernice comments: rather have a global variable NUM_PLAYERS instead of a magic number      
-      for (i = 1; i < NUM_PLAYERS-1; i++) {
+	for (i = 1; i < NUM_PLAYERS-1; i++) {
 //Bernice comments: Brady has implemented a findPathDist() function that only returns the distance without having to pass in an array pointer.
 //    rather use findPathDist() to eliminate any possibility of seg faults because we know how annoying C is when this happens
-         tempMinimum = findPathDist(gameMap, whereIs(gameState, PLAYER_DRACULA), whereIs(gameState, i));
-         if (tempMinimum < minimum) {
-            minimum = tempMinimum;
-            closestHunter = i;
-         }
-      }
+		tempMinimum = findPathDist(gameMap, whereIs(gameState, PLAYER_DRACULA), whereIs(gameState, i));
+		if (tempMinimum < minimum) {
+			minimum = tempMinimum;
+			closestHunter = i;
+		}
+	}
 
-      // Finding location that is furthest away from the closest hunter
-      bestPlay = paths[0];
-      minimum = findPathDist(gameMap, closestHunter, paths[0]);
-      for (i = 1; i < numLocations; i++) {
-         tempMinimum = findPathDist(gameMap, closestHunter, paths[i]);
-         if (tempMinimum < minimum) {
-            minimum = tempMinimum;
-            bestPlay = paths[i];
-         }
-      }
+	  // Finding location that is furthest away from the closest hunter
+	bestPlay = paths[0];
+	minimum = findPathDist(gameMap, closestHunter, paths[0]);
+	for (i = 1; i < numLocations; i++) {
+		tempMinimum = findPathDist(gameMap, closestHunter, paths[i]);
+		if (tempMinimum < minimum) {
+			minimum = tempMinimum;
+			bestPlay = paths[i];
+		}
+	}
 //Bernice comments: what if a hunter is exactly one location away from Dracula? Do we want to attack?
-   } else {
-      bestPlay = CASTLE_DRACULA;
-   }
+   // If no possible locations to move to
+	} else if (numLocations == 0) {
+		bestPlay = CASTLE_DRACULA;
+   // Start of the game
+	} else {
+		bestPlay = ATHENS;
+	}
 
 
    registerBestPlay(idToName(bestPlay), "");
@@ -63,9 +66,9 @@ void decideDraculaMove(DracView gameState)
    /*
    
    Random Move Immediately: Everything
-      remeber to not make illegal moves
-      Not ocean
-      
+	  remeber to not make illegal moves
+	  Not ocean
+	  
    Don't move within 1 of a hunter
    
    If below 10 life, move towards castle
@@ -89,6 +92,6 @@ void decideDraculaMove(DracView gameState)
 
    int randIndex = (rand()%(*numLocations-1));
    return possibleLocs[randIndex];
-}*/
-
+}
+*/
 
